8cbf5d450cb495df94805746ea7a2af16b6a4243
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2024-03-06 21:27:45 +0000

    input for date

---
+++ jbjarnas_lab1/lab1.py
@@ -0,0 +1,10 @@
+import datetime
+
+print("starting")
+current_year = int(input("Year: "))
+current_month = int(input("Month: "))
+current_day = int(input("Day: "))
+current_hour = int(input("Hour: "))
+current_minute = int(input("Minute: "))
+current_second = int(input("Second: "))
+current_microsecond = int(input("Microsecond: "))
